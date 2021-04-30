16af296105504dd21976cb9d9f98779175e477f0
Lois-Anne <63202227+Lois-Anne@users.noreply.github.com>
2021-04-29 21:33:18 -0500

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Lois-Anne Hall on 28/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
